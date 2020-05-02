320c97133dd9c73b4089af43aae5e54c60ae5648
herry <yuandeng@aliyun.com>
2020-05-02 23:49:39 +0800

    fix config bug

--- src/Providers/SnowflakeServiceProvider.php
+++ src/Providers/SnowflakeServiceProvider.php
@@ -10,7 +10,7 @@ class SnowflakeServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(
-            dirname(__DIR__) . '../config/snowflake.php', 'snowflake'
+            dirname(__DIR__) . '/../config/snowflake.php', 'snowflake'
         );
         $this->app->singleton('snowflake', function ($app) {
             return new Snowflake($app);
@@ -21,7 +21,7 @@ class SnowflakeServiceProvider extends ServiceProvider
     {
         if (!file_exists(config_path('snowflake.php'))) {
             $this->publishes([
-                dirname(__DIR__) . '../config/snowflake.php' => config_path('snowflake.php'),
+                dirname(__DIR__) . '/../config/snowflake.php' => config_path('snowflake.php'),
             ], 'config');
         }
     }
